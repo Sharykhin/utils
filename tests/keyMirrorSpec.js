552e22d9e3595c91d7ae61dca5cf8fb98f6c6c88
chapal <siarhei.sharykhin@gmail.com>
2015-10-04 21:55:46 +0300

    add deeply possiblity

--- tests/keyMirrorSpec.js
+++ tests/keyMirrorSpec.js
@@ -26,7 +26,53 @@ describe('keyMirror function', function() {
 	});
 
 	it('should not change value if it was specified', function(){
-		
+		var obj = {
+					ADD: null,
+					TODO: {
+						CREATE: null,
+						UPDATE: null
+						},
+					DELETE:'foo'
+					};
+
+		var result = keyMirror(obj);
+		assert.deepEqual(result, {ADD:'ADD',TODO:{ CREATE:'TODO_CREATE', UPDATE:'TODO_UPDATE' }, DELETE:'foo'});
+
+	});
+
+	it('should bo ably to handle very deep objects', function() {
+			var obj = {
+					ADD: null,
+					TODO: {
+						CREATE: null,
+						UPDATE: null,
+						MAIL: {
+							SEND:null,
+							GET: null,
+							META: {
+								AUTHOR:null,							
+							}
+						}
+						},
+					DELETE:'foo'
+					};
+			var result = keyMirror(obj);
+			var resultObj = {
+				ADD:'ADD',
+				TODO: {
+					CREATE:'TODO_CREATE',
+					UPDATE:'TODO_UPDATE',
+					MAIL:{
+						SEND:'TODO_MAIL_SEND',
+						GET:'TODO_MAIL_GET',
+						META: {
+							AUTHOR: 'TODO_MAIL_META_AUTHOR'							
+						}
+					}
+				},
+				DELETE:'foo'
+			};	 
+			assert.deepEqual(result, resultObj);
 	});
 
 });
--- utils/keyMirror.js
+++ utils/keyMirror.js
@@ -15,25 +15,44 @@ function keyMirror(obj) {
             var isObject = false;  
            
 			if (obj[key] !== null && typeof obj[key] === 'object')	{            
-				isObject = true;             
-				this.assign(obj[key], key);
-                
-			}   
+				isObject = true; 
+                var initalizeNs = ns ? ns + '_' + key : key;
+				this.assign(obj[key], initalizeNs);   
+                initalizeNs=undefined;
+			} 
             
             if (ns) {             
-              this.assignNS(ns);            
-              this.mirrorObj[ns][key]=ns + '_' + key;
-            } else if(!isObject) {               
+              this.assignNS(ns,key);                
+            } else if(!isObject && obj[key] === null) {               
               this.mirrorObj[key] = key;             
-            } 
+            } else if(!isObject) {
+              this.mirrorObj[key] = obj[key]; 
+            }
 		}		
 	};
+  
     
-    Mirror.prototype.assignNS = function(ns) {
-      if (this.mirrorObj[ns] === undefined) {
-        this.mirrorObj[ns] = {};
-      }     
+    
+    Mirror.prototype.assignNS = function(ns,key) {       
+      var nsArr = ns.split('_');        
+      this.extendMirrorObj(nsArr,this.mirrorObj,key,ns);
     };	
+  
+    Mirror.prototype.extendMirrorObj = function(properties, obj,value,ns) {
+        var key = properties.shift();   
+                               
+        if (obj[key] === undefined) {          
+            obj[key] = {};
+        } 
+        if(properties.length === 0) {          
+          if(obj[key][value] === undefined) {
+            obj[key][value] = ns + '_' + value;           
+          }
+          return;
+        }
+        this.extendMirrorObj(properties,obj[key],value,ns);
+    };
+  
     var mirror = new Mirror(obj);
 	return mirror.mirrorObj;
 }
